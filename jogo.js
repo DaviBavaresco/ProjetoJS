f581390950bf3611758e34b63b74937f3c789afe
DaviBavaresco <daviantoniobavaresco@gmail.com>
2022-10-21 17:43:20 -0300

    commit 4
    melhorei a forma com que o personagem é montado, para deixar o codigo melhor e mais limpo

--- jogo.js
+++ jogo.js
@@ -5,18 +5,31 @@ const canvas = document.querySelector('canvas');
 // jogo vai ser 2D >
 const contexto = canvas.getContext('2d');
 
-//função feita para montar o FPS (frames por segundo, imagen lisa)
-function loop(){
+// transformo o nosso personagem em um objeto
+const FlappyByrd = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 34,
+    altura: 24,
+    x: 10,
+    y: 50,
 
-// começo a recortar as imagens 1
-contexto.drawImage(
-    sprites, //escolho o arquivo
-    0, 0, //distancias da borda da primeira imagem
-    34, 24,  // o tamanho ocupado pela imagem para recortar
-    10, 50, //aonde vai aparecer na tela
-    34, 24, //qual o tamanho dentro do canva
-);
-requestAnimationFrame(loop);
+    desenha() {
+        contexto.drawImage(
+            sprites, //escolho o arquivo
+            FlappyByrd.spriteX, FlappyByrd.spriteY, //distancias da borda da primeira imagem
+            FlappyByrd.largura, FlappyByrd.altura,  // o tamanho ocupado pela imagem para recortar
+            FlappyByrd.x, FlappyByrd.y, //aonde vai aparecer na tela
+            FlappyByrd.largura, FlappyByrd.altura //qual o tamanho dentro do canva
+        );
+    }
+}
+//função feita para montar o FPS (frames por segundo, imagen lisa)
+function loop() {
+    FlappyByrd.desenha();
+
+
+    requestAnimationFrame(loop);
 }
 loop(); //chamo a função
 
