e23139eec69015f5ce01f86e63ed17b58e584594
DaviBavaresco <daviantoniobavaresco@gmail.com>
2022-10-21 18:03:47 -0300

    montagem chão

--- jogo.js
+++ jogo.js
@@ -24,10 +24,42 @@ const FlappyByrd = {
         );
     }
 }
+
+
+
+// preparação do chão
+const chao = {
+spriteX: 0,
+spriteY: 607,
+largura:224,
+altura:112,
+x: 0,
+y: canvas.height-112, // empurro ele todo para baixo, subo só a altura dele deixando ele perfeito no canva
+
+desenha(){
+    contexto.drawImage( //primeiro chao
+      sprites,
+      chao.spriteX,chao.spriteY,
+      chao.largura,chao.altura,
+      chao.x,chao.y,
+      chao.largura,chao.altura, 
+    );
+    contexto.drawImage( //segundo chao duplicado
+      sprites,
+      chao.spriteX,chao.spriteY,
+      chao.largura,chao.altura,
+      (chao.x+chao.largura),chao.y, //esse metodo usado para empurar a imagem pego ele grudado na parede e empuro ele usando a largura para cobrir a tela
+      chao.largura,chao.altura, 
+    );
+}
+
+
+}
+
 //função feita para montar o FPS (frames por segundo, imagen lisa)
 function loop() {
     FlappyByrd.desenha();
-
+    chao.desenha();
 
     requestAnimationFrame(loop);
 }
